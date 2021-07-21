cf1a0fc4fe912378caae3cf0c0c20a7185c11ace
tuananh_nguyen_vn <tuananh_nguyen_vn@ascendcorp.com>
2021-07-21 16:49:52 +0700

    Feign

--- src/main/java/com/anhnt/configuration/ConfigurationApplication.java
+++ src/main/java/com/anhnt/configuration/ConfigurationApplication.java
@@ -2,8 +2,12 @@ package com.anhnt.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableFeignClients
 public class ConfigurationApplication {
 
 	public static void main(String[] args) {
